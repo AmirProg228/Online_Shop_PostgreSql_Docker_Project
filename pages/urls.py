7f152b5bc5ffd960cb1ca49fc52626789e5c9886
Amir <tabanamir2012@gmail.com>
2022-11-09 17:24:51 +0330

    add login and sign up form

--- pages/urls.py
+++ pages/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HomePageView.as_view(), name='homepage'),
-    path('aboutus/', views.AboutUsPageView.as_view(), name='about-us'),
+    path('', views.HomePageView.as_view(), name='home'),
+    path('aboutus/', views.AboutUsPageView.as_view(), name='aboutus'),
 
 ]
